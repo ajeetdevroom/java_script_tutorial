0264a8825887bfddc803b1892ba08e32bdaa6c6e
Ajeet Kumar <a.kumar@ipresourcing.in>
2020-10-17 11:25:46 +0530

    chnage

--- jstuts5.js
+++ jstuts5.js
@@ -1,1 +1,7 @@
 console.log('javaScript type conversion and type coersion');
+let myVar;
+myVar=45;
+let x='563475.78';
+let con= parseInt(x);
+console.log(con);
+console.log(typeof con,con.toFixed(5));
---
+++ jstuts6.js
@@ -0,0 +1,20 @@
+console.log('Strings: Properties, Methods & Template Literals in JavaScript');
+let name = 'Ajeet';
+// const greeting = 'Good morning';
+// console.log(greeting +" "+name);
+let htmlContens= `<h1>Heading is one</h1> ${name}  <p>this paragisraph</p>`;
+// console.log(htmlContens.toUpperCase());
+// console.log(htmlContens);
+// console.log(htmlContens.charAt('12'));
+// console.log(htmlContens.includes('one'));
+// console.log(htmlContens.substring(0,20));
+// console.log(htmlContens.slice(0,20));
+// console.log(htmlContens);
+document.body.innerHTML=htmlContens;
+// console.log(htmlContens.split(' '));
+// console.log(name.replace('Ajeet','Chaman'));
+// template litteral
+
+
+
+
